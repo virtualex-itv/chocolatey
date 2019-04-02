23869aa9d3b497f210cf89b652433fdf2a50255f
Alex Lopez <info@ithinkvirtual.com>
2019-04-02 10:50:10 -0400

    Update ChocolateyInstall.ps1

--- _packages/vmware-powercli-psmodule/tools/ChocolateyInstall.ps1
+++ _packages/vmware-powercli-psmodule/tools/ChocolateyInstall.ps1
@@ -13,11 +13,12 @@ If ( $Provider.Name -notmatch "NuGet" ) {
 # This will install the VMware.PowerCLI Module
 $ModuleVersion = ( Get-Module "VMware.PowerCLI" ).Version
 
-If ( $ModuleVersion -ge "6.5.1.5377412" -and $ModuleVersion -lt "11.2.0.12483598" ) { Update-Module "VMware.PowerCLI" -RequiredVersion "11.2.0.12483598" }
-Else { Find-Module -Name "VMware.PowerCLI" | Install-Module -RequiredVersion "11.2.0.12483598" -Scope "AllUsers" -AllowClobber -Force }
+If ( $ModuleVersion -ge "6.5.1.5377412" -and $ModuleVersion -lt "11.2.0.12780525" ) { Update-Module "VMware.PowerCLI" -RequiredVersion "11.2.0.12780525" }
+Else { Find-Module -Name "VMware.PowerCLI" | Install-Module -RequiredVersion "11.2.0.12780525" -Scope "AllUsers" -AllowClobber -Force }
 
 # This will create a Desktop Shortcut
 $Path = $env:Public + "\Desktop\VMware PowerCLI.lnk"
+$Path2 = $env:ProgramData + "\Microsoft\Windows\Start Menu\Programs\VMware PowerCLI.lnk"
 $TargetPath = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
 $WorkingDir = $env:HomeDrive + "\"
 $Arguments = '-noe -c "Import-Module VMware.PowerCLI"'
@@ -35,3 +36,16 @@ If ( ! ( Test-Path $Path )  ) {
 		-WindowStyle '1' `
 		-RunAsAdmin
 	}
+
+If ( ! ( Test-Path $Path2 )  ) { 
+	Install-ChocolateyShortcut `
+		-ShortcutFilePath $Path2 `
+		-TargetPath $TargetPath `
+		-WorkingDirectory $WorkingDir `
+		-Arguments $Arguments `
+		-IconLocation $Icon `
+		-Description $Desc `
+		-WindowStyle '1' `
+		-RunAsAdmin
+	}
+
